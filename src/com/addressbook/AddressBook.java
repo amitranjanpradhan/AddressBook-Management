d40b1df4dbda4b6efd9636eb88989bfb18e24ffc
amitranjan <amitranjanpradhan456@gmail.com>
2021-05-28 07:17:22 +0530

    [Amit] Added Viewing Option In UC9

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -1,103 +1,52 @@
 package com.addressbook;
 
-/**
- * Here i have imported Scanner to take user input.
- * imported ArrayList to store the values in an ArrayList.
- */
 public class AddressBook {
-    public String firstname, lastname;
-    public String address, city, state;
-    public int zip;
-    public long phoneNumber;
-    public String email;
-
+    public String BookName;
+    public String FirstName;
+    public String LastName;
+    public String Address;
+    public String City;
+    public int Zip;
+    public String State;
+    public long PhoneNumber;
+    public String Email;
     /**
      * This is a parameterized constructor having the given parameters.
-     * @param firstname
-     * @param lastname
-     * @param address
-     * @param city
-     * @param state
-     * @param zip
-     * @param phonenumber
-     * @param email
+     * @param BookName
+     * @param FirstName
+     * @param LastName
+     * @param Address
+     * @param City
+     * @param Zip
+     * @param State
+     * @param PhoneNumber
+     * @param Email
      */
-    public AddressBook(String firstname, String lastname, String address, String city, String state, int zip, long phonenumber, String email) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phonenumber;
-        this.email = email;
+    public AddressBook(String BookName, String FirstName, String LastName, String Address, String City, int Zip,
+                       String State, long PhoneNumber, String Email) {
+        this.BookName = BookName;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Address = Address;
+        this.City = City;
+        this.Zip = Zip;
+        this.State = State;
+        this.PhoneNumber = PhoneNumber;
+        this.Email = Email;
     }
-
     /**
-     * these get methods are used to get the values according to the user input.
-     * @return
+     * this constructor will work if we'll not pass any parameters.
      */
-    public String getFirstName() {
-        return firstname;
-    }
-    public void setFirstName(String firstName) {
-        this.firstname = firstName;
-    }
-    public String getLastName() {
-        return lastname;
-    }
-    public void setLastName(String lastName) {
-        this.lastname = lastName;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getState() {
-        return state;
-    }
-    public void setState(String state) {
-        this.state = state;
-    }
-    public int getZip() {
-        return zip;
-    }
-    public void setZip(int zip) {
-        this.zip = zip;
-    }
-    public long getPhoneNo() {
-        return phoneNumber;
-    }
-    public void setPhoneNo(long phno) {
-        this.phoneNumber = phno;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public AddressBook(){
 
+    }
     /**
-     * This method will display the data of the addressbook.
+     * this method will return all initialised values.
+     * @return string.
      */
-    public void printContactDetails() {
-        System.out.println("Firstname : " + getFirstName());
-        System.out.println("Lastname : " + getLastName());
-        System.out.println("Address : " + getAddress());
-        System.out.println("City : " + getCity());
-        System.out.println("State : " + getState());
-        System.out.println("Zip : " + getZip());
-        System.out.println("PhoneNumber : " + getPhoneNo());
-        System.out.println("Email : " + getEmail());
-
+    @Override
+    public String toString() {
+        return BookName + " " + FirstName + " " + LastName + " " + Address + " " + City + " " + Zip + " " + State + " "
+                + PhoneNumber + " " + Email;
     }
 }
